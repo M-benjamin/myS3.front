5c50fb876fcb97d9dad247e988a3c682fb0e4f5a
benjaminmballa <mballabenjamin@yahoo.fr>
2018-12-01 01:53:51 +0100

    Template integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Route,
+  Router,
+  Switch,
+  Redirect,
+  withRouter,
+  Link
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import Dashboard from "./layout/Dashboard/Dashboard";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Link to="/">Login</Link>
+        <Link to="/dashboard">Dashbord</Link>
+        <Link to="/register">Register</Link>
+
+        <Route exact path="/" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/register" component={Register} />
       </div>
     );
   }
